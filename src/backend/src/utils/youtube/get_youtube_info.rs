62e3c5abb2126f8eea42fc0fe7c1a5cee9895173
simeonnv <simmeon.nv@gmail.com>
2025-04-01 01:51:04 +0300

    getting yt song artist and title

---
+++ src/backend/src/utils/youtube/get_youtube_info.rs
@@ -0,0 +1,24 @@
+use crate::error::Error;
+use rusty_ytdl::{Video, VideoOptions, VideoSearchOptions};
+
+pub struct YtInfo {
+    pub title: String,
+    pub artist: String,
+    pub id: String,
+}
+
+pub async fn get_youtube_info(url: String) -> Result<YtInfo, Error> {
+    let video_options = VideoOptions {
+        filter: VideoSearchOptions::Audio,
+        ..Default::default()
+    };
+    let video = Video::new_with_options(&url, video_options).unwrap();
+
+    let info = video.get_info().await.unwrap();
+
+    Ok(YtInfo {
+        title: info.video_details.title,
+        artist: info.video_details.owner_channel_name,
+        id: info.video_details.video_id,
+    })
+}
--- src/backend/src/utils/youtube/mod.rs
+++ src/backend/src/utils/youtube/mod.rs
@@ -1,1 +1,2 @@
 pub mod get_youtube_audio;
+pub mod get_youtube_info;
